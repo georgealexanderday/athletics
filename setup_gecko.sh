d04f7ef0ffbece3f92283e609469be129d35c6f4
George Alexander Day <georgealexanderday1@gmail.com>
2022-10-08 18:13:44 +0000

    nice

--- setup_gecko.sh
+++ setup_gecko.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-
+sudo apt update
+sudo apt install firefox
 wget https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz
 tar xvzf geckodriver-v0.31.0-linux64.tar.gz
 mv geckodriver ~/.local/bin/
@@ -7,5 +8,5 @@ chmod +x ~/.local/bin/geckodriver
 rm geckodriver-*
 
 if [ -d "$HOME/.local/bin" ] ; then
-    PATH="$HOME/.local/bin:$PATH"
+    PATH="$PATH:$HOME/.local/bin"
 fi
